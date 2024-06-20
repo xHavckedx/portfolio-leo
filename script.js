d2222139700a275af5488eec015eb6649c11d646
Leo Gomez <leo.gomez@ackstorm.com>
2024-06-20 13:05:57 +0200

    feat(myself): Adding some data to the portfolio

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function mostrarOcultarMenu(){
 function seleccionar(){
     //oculto el menu una vez que selecciono una opcion
     document.getElementById("nav").classList = "";
-    menuVisible = false;
+    menuVisible = true;
 }
 //Funcion que aplica las animaciones de las habilidades
 function efectoHabilidades(){
@@ -21,11 +21,11 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
+        habilidades[0].classList.add("aws-gcp-azure");
+        habilidades[1].classList.add("cicd");
+        habilidades[2].classList.add("qa");
+        habilidades[3].classList.add("iac");
+        habilidades[4].classList.add("programacion");
         habilidades[5].classList.add("comunicacion");
         habilidades[6].classList.add("trabajo");
         habilidades[7].classList.add("creatividad");
